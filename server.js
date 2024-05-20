df070aa1c400956d5e74d739d4f02f9a9dddd0a7
GonCar <31064162+GonCar@users.noreply.github.com>
2024-05-20 14:56:55 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const jwt = require('jswonwebtoken');
+require('dotenv').config();
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+var currentKey = "";
+var currentPassword = "";
+
+const PORT = 8080;
+const SECRET_KEY = process.env.TOKEN;
+
+app.get('/', (req, res) =>{
+    res.redirect("/identify");
+});
+
+app.post('/identify', (req, res) => {
+    //authenticate
+    const username = req.body.password;
+    const token = jwt.sign(username, SECRET_KEY);
+    currentKey = token;
+    currentPassword = username;
+    res.redirect("/granted");
+})
+
+app.get('/granted', (req, res) => {
+    res.render("start.ejs");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server active on port: http://localhost:${PORT}/`);
+});
